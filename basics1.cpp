40d0ae5d65a73b8dba2898bc2c9c38ec002c8a22
jaideepgoyal06 <jaideepgoyal51@gmail.com>
2025-08-27 19:32:39 +0530

    Create basics1.cpp

---
+++ basics1.cpp
@@ -0,0 +1,8 @@
+https://www.geeksforgeeks.org/problems/odd-or-even3618/1
+class Solution {
+public:
+    bool isEven(int n) {
+        // Using modulus operator
+        return n % 2 == 0;
+    }
+};
